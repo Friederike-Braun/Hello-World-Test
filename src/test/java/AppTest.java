1ce3b5ee1e47d1e5fce23651ee50321e27cbf08c
Friederike Braun <fbraun@haeger-consulting.de>
2021-11-19 08:51:43 +0100

    added example test case that always succeeds

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,5 +1,18 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
+
+    @Test
+    public void exampleTest_AlwaysSucceeds(){
+
+        // given
+        boolean input = true;
+
+        // when
+        // then
+        assertTrue(input);
+    }
 
 }
